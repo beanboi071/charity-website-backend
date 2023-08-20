dc2d86b2324727095b1ac73d54a7d29ad2077a53
rojanshakya071 <rojan@riddhasoft.com>
2023-08-20 11:26:22 +0545

    api added to open files

---
+++ charity-website-backend/Common/Services/CommonService.cs
@@ -0,0 +1,27 @@
+﻿namespace charity_website_backend.Common.Services
+{
+    public class CommonService:ICommonService
+    {
+        public async Task GetImageFile(string fileName, HttpContext context)
+        {
+            // Define a custom directory name to store images
+            string customDirectoryName = "uploads";
+            string imageDirectory = Path.Combine(AppContext.BaseDirectory, customDirectoryName);
+
+
+            // Create the custom directory if it does not exist
+            Directory.CreateDirectory(imageDirectory);
+
+            string filePath = Path.Combine(imageDirectory, fileName);
+
+            if (File.Exists(filePath))
+            {
+                await context.Response.SendFileAsync(filePath);
+            }
+            else
+            {
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+            }
+        }
+    }
+}
---
+++ charity-website-backend/Common/Services/ICommonService.cs
@@ -0,0 +1,7 @@
+﻿namespace charity_website_backend.Common.Services
+{
+    public interface ICommonService
+    {
+        Task GetImageFile(string fileName, HttpContext context);
+    }
+}
